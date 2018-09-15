36adfc94c4c17aae50da8000a4be5b8051239148
lozenia64 <42924631+lozenia64@users.noreply.github.com>
2018-09-16 01:33:46 +0900

    directory arrangement

---
+++ Sort/SelectionSort.cpp
@@ -0,0 +1,13 @@
+// lozenia64
+// https://blog.naver.com/ljy9378/221357120126
+// Algorithm source cod for c++
+
+void SelectionSort(int *arr, int begin, int end) {
+	for (int i = begin; i < end; i++) {
+		int tmp = i;
+		for (int j = i + 1; j <= end; j++)
+			if (arr[tmp] > arr[j]) tmp = j;
+		if (tmp != i) swap(arr[i], arr[tmp]);
+	}
+}
+// SelectionSort(Selection, 0, SIZE - 1);
