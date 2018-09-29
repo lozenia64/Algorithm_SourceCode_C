dd11e7df1838679c71941e8ab5c00619c53b26f0
lozenia64 <lozenia64@gmail.com>
2018-09-29 14:28:49 +0900

    SelectionSort

--- Sort/SelectionSort.cpp
+++ Sort/SelectionSort.cpp
@@ -9,6 +9,7 @@ void SelectionSort(int *arr, int begin, int end) {
 		int tmp = i;
 		//i+1부터 끝까지 확인
 		for (int j = i + 1; j <= end; j++)
+			//arr[j]가 더 작으면 앞으로 tmp 변경. 즉 tmp가 최소값을 가르킴
 			if (arr[tmp] > arr[j]) tmp = j;
 		if (tmp != i) swap(arr[i], arr[tmp]);
 	}
