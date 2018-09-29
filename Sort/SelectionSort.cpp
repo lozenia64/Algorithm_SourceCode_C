45e494d8c5b8d40e69ac7493dafcb9397c399e16
lozenia64 <lozenia64@gmail.com>
2018-09-29 14:26:46 +0900

    SelectionSort

--- Sort/SelectionSort.cpp
+++ Sort/SelectionSort.cpp
@@ -5,7 +5,9 @@
 void SelectionSort(int *arr, int begin, int end) {
 	//처음부터 쭉 반복
 	for (int i = begin; i < end; i++) {
+		//tmp에 i값을 저장
 		int tmp = i;
+		//i+1부터 끝까지 확인
 		for (int j = i + 1; j <= end; j++)
 			if (arr[tmp] > arr[j]) tmp = j;
 		if (tmp != i) swap(arr[i], arr[tmp]);
