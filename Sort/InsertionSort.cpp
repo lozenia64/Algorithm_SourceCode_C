5dae5b3386bddb735c86336ebcaea66247f61bfc
lozenia64 <lozenia64@gmail.com>
2018-10-02 00:41:46 +0900

    InsertionSort

--- Sort/InsertionSort.cpp
+++ Sort/InsertionSort.cpp
@@ -3,8 +3,11 @@
 // Algorithm source cod for c++
 
 void InsertionSort(int *arr, int begin, int end) {
+	//arr[0]~마지막까지 확인
 	for (int i = begin + 1; i <= end; i++) {
+		//i반째 원소를 v에 저장
 		int j, v = arr[i];
+		//i부터 앞으로 확인
 		for (j = i; j > begin && arr[j - 1] > v; j--)
 			arr[j] = arr[j - 1];
 		if (i != j) arr[j] = v;
