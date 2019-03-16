71d25094294471d85672f945158e13217ffb35ef
lozenia64 <lozenia64@gmail.com>
2019-03-16 15:15:41 +0900

    ccw

--- Algorithm/ccw.cpp
+++ Algorithm/ccw.cpp
@@ -1,8 +1,8 @@
+
 typedef pair<int, int> P;
 int ccw(P a, P b, P c) {
 	return (a.x*b.y + b.x*c.y + c.x*a.y) - (a.y*b.x + b.y*c.x + c.y*a.x);
 }
-
 
 int ccw(int x1, int y1, int x2, int y2, int x3, int y3) {
 	return (x1*y2 + x2*y3 + x3*y1) - (y1*x2 + y2*x3 + y3*x1);
